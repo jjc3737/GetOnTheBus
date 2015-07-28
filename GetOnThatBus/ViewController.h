98e26c7d72b3d4528c562230f575ee964376b7c4
jjc3737 <Jane@Jaehees-MacBook-Pro.local>
2015-07-28 14:00:11 -0700

    Initial Commit

---
+++ GetOnThatBus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetOnThatBus
+//
+//  Created by Jaehee Chung on 7/28/15.
+//  Copyright (c) 2015 Jaehee Chung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
